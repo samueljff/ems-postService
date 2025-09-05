ddcee774ed632b4e256387cfbebef3d58bbd3057
samueljff <samueljosef3@gmail.com>
2025-09-04 23:00:38 -0300

    Inital project

---
+++ src/test/java/com/fonseca/algaposts/postService/PostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fonseca.algaposts.postService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
